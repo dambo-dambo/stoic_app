3cd60e65577b6d38658d394198b4c76f3408eca9
Denis <74350269+dambo-dambo@users.noreply.github.com>
2021-03-10 19:19:33 +0300

    change form style add_stoic html views

--- stoic/forms.py
+++ stoic/forms.py
@@ -3,7 +3,12 @@ from .models import Month
 
 
 class StoicForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    month = forms.ModelChoiceField(queryset=Month.objects.all())
+    title = forms.CharField(max_length=150, label='Заголовок', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 5
+    }))
+    is_published = forms.BooleanField(label='Опубликовать?', initial=True)
+    month = forms.ModelChoiceField(empty_label='Выберите месяц', label='Месяц',
+                                      queryset=Month.objects.all(),
+                                      widget=forms.Select(attrs={"class": "form-control"}))
--- stoic/views.py
+++ stoic/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .forms import StoicForm
@@ -20,14 +20,21 @@ def get_month(request, month_id):
     month = Month.objects.get(pk=month_id)
     return render(request, 'stoic/month.html', {'stoic': stoic, 'month': month})
 
+
 def view_stoic(request, stoic_id):
     #stoic_item = Stoic.objects.get(pk=news_id)
     stoic_item = get_object_or_404(Stoic, pk=stoic_id)
     return render(request, 'stoic/view_stoic.html', {"stoic_item": stoic_item})
 
+
 def add_stoic(request):
     if request.method == 'POST':
-        pass
+        form = StoicForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            #распаковка словаря
+            stoic = Stoic.objects.create(**form.cleaned_data)
+            return redirect(stoic)
     else:
         form = StoicForm()
     return render(request, 'stoic/add_stoic.html', {'form': form})
