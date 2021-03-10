1d1229bebbdfde0da56a05bcdc98eee00b3b8979
Denis <74350269+dambo-dambo@users.noreply.github.com>
2021-03-10 18:08:27 +0300

    add css,js style
    add STATIC_ROOT STATICFILES_DIRS

--- stoic_app/settings.py
+++ stoic_app/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'stoic_app/static'),
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
