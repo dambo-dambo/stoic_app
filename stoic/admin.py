0e45dd11c6e87d10da40f54a21d278910de1fa33
Denis <74350269+dambo-dambo@users.noreply.github.com>
2021-03-10 10:38:14 +0300

    add model month add admin features add filter

--- stoic/admin.py
+++ stoic/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Stoic
+from .models import Stoic, Month
 
 
 class StoicAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'month', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'month')
+
+class MonthAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 admin.site.register(Stoic, StoicAdmin)
-
+admin.site.register(Month, MonthAdmin)
--- stoic/models.py
+++ stoic/models.py
@@ -8,11 +8,24 @@ class Stoic(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    month = models.ForeignKey('Month', on_delete=models.PROTECT, null=True, verbose_name='Месяц')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
+        verbose_name = 'Высказывание'
+        verbose_name_plural = 'Дни'
+        ordering = ['id']
+
+
+class Month(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование месяца')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Месяц'
+        verbose_name_plural = 'Месяцы'
         ordering = ['id']
